77f841904d2bc56e3e44894a6439dbe053f5a241
Festus <festusasiyanbi80@gmail.com>
2026-02-15 00:37:35 -0400

    fix: resolved checkout issues on mobile

--- src/hooks/commerce/useOrders.ts
+++ src/hooks/commerce/useOrders.ts
@@ -31,8 +31,7 @@ export function useCheckoutOrder(cartId: string) {
 
     onError: (err) => {
       const { translate } = useLanguageStore.getState();
-      // error(translate(interpretOrderError(err)));
-      error(err.message);
+      error(translate(interpretOrderError(err)));
     },
   });
 }
