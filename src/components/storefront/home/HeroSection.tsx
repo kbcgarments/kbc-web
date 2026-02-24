e4fcf19e890c269ad452775989152d7b1b621f13
Festus <festusasiyanbi80@gmail.com>
2026-02-23 20:01:01 -0400

    chore: removed base64 from hero image and added unoptimized prop

--- src/components/storefront/home/HeroSection.tsx
+++ src/components/storefront/home/HeroSection.tsx
@@ -45,9 +45,8 @@ export default function HeroSection({
         fetchPriority="high"
         sizes="100vw"
         quality={75}
-        placeholder="blur"
-        blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
         className="object-cover"
+        unoptimized
       />
 
       {/* Overlays */}
